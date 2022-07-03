bbc80b13f62024fda7e07b0afd15fe989d3bcd56
Journeyman32 <bryan@coderbox.co>
2022-07-03 01:17:52 -0500

    init commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//Print out current date and time
+Console.WriteLine(DateTime.Now);
+//Call a new method to print out current date and time
+PrintDateTime();
+
+//Create a new method called PrintDateTime
+void PrintDateTime()
+{
+    Console.WriteLine(DateTime.Now);
+}
